4762582f22bbe846895a95f82fab673929b3a164
rineun <sy880808@gmail.com>
2022-03-16 17:07:13 +0900

    spring version

--- src/main/java/spring/core/AppConfig.java
+++ src/main/java/spring/core/AppConfig.java
@@ -1,5 +1,7 @@
 package spring.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import spring.core.discount.DiscountPolicy;
 import spring.core.discount.FixDiscountPolicy;
 import spring.core.discount.RateDiscountPolicy;
@@ -7,19 +9,26 @@ import spring.core.member.*;
 import spring.core.order.OrderService;
 import spring.core.order.OrderServiceImpl;
 
+//설정을 구성
+@Configuration
 public class AppConfig {
+    // 스프링 컨테이너에 스프링 빈으로 등록
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
