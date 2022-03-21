5cfe4dfb9fdbf4855d22ab8f24d19bb893e86127
rineun <sy880808@gmail.com>
2022-03-21 16:51:31 +0900

    spring autowired

--- src/main/java/spring/core/discount/FixDiscountPolicy.java
+++ src/main/java/spring/core/discount/FixDiscountPolicy.java
@@ -1,8 +1,12 @@
 package spring.core.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 import spring.core.member.Grade;
 import spring.core.member.Member;
 
+@Component
+@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount =1000; //1000원만 할인받음
--- src/main/java/spring/core/discount/RateDiscountPolicy.java
+++ src/main/java/spring/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,16 @@
 package spring.core.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import spring.core.annotataion.MainDiscountPolicy;
 import spring.core.member.Grade;
 import spring.core.member.Member;
 
 @Component
+@Qualifier("mainDiscountPolicy")
+@Primary
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
     private int discountPercent =10; //10%
     @Override
