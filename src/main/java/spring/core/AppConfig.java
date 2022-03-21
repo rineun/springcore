c7c524f133e5f0d0b935908bc3fa845414148f32
rineun <sy880808@gmail.com>
2022-03-21 13:44:55 +0900

    spring singleton

--- src/main/java/spring/core/AppConfig.java
+++ src/main/java/spring/core/AppConfig.java
@@ -14,22 +14,28 @@ import spring.core.order.OrderServiceImpl;
 public class AppConfig {
     // 스프링 컨테이너에 스프링 빈으로 등록
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
+        //1번
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public OrderService orderService(){
+    public OrderService orderService() {
+        //1번
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
-    public MemberRepository memberRepository(){
+    public MemberRepository memberRepository() {
+        //2번? 3번 ?
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
-    public DiscountPolicy discountPolicy(){
+    public DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
